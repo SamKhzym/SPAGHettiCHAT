b881c65c35122cce8d8beb014289f23f94abda18
Sam <smryk7@gmail.com>
2024-04-01 22:51:23 -0400

    added functionality for dynamically adding a chat to chat window. works as proof of concept butneed to refactor.

--- SpaghettiChatClient/app/src/main/java/com/example/spaghettichat/chatmanager/ChatManagerActivity.java
+++ SpaghettiChatClient/app/src/main/java/com/example/spaghettichat/chatmanager/ChatManagerActivity.java
@@ -1,13 +1,18 @@
 package com.example.spaghettichat.chatmanager;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.ui.AppBarConfiguration;
 
 import com.example.spaghettichat.databinding.ActivityChatManagerBinding;
@@ -31,9 +36,33 @@ public class ChatManagerActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+                LinearLayout chatList = (LinearLayout)findViewById(R.id.chatList);
+                int count = chatList.getChildCount();
+
+                LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View newView = inflater.inflate(R.layout.sample_chat_view, null);
+                TextView employeeTextView = (TextView) newView.findViewById(R.id.employee_name);
+                employeeTextView.setText("Samuel Khzym");
+
+                TextView messageTextView = (TextView) newView.findViewById(R.id.message_sample);
+                messageTextView.setText("Have you ever heard the tragedy of darth plagueis the wise? it's not a story the jedi would tell you.");
+
+                TextView datetime = (TextView) newView.findViewById(R.id.timestamp);
+                datetime.setText("January 31, 9:30 PM");
+
+                chatList.addView(newView);
+
+                Snackbar.make(view, "I have " + count + " chats!", Snackbar.LENGTH_LONG)
                         .setAnchorView(R.id.fab)
                         .setAction("Action", null).show();
+
+                for (int i = 0; i < count; i++) {
+                    View child = chatList.getChildAt(i);
+                    TextView nameView = (TextView)child.findViewById(R.id.employee_name);
+                    System.out.println(nameView);
+                }
+
             }
         });
     }
---
+++ SpaghettiChatClient/app/src/main/java/com/example/spaghettichat/messagemanager/MessageManagerActivity.java
@@ -0,0 +1,22 @@
+package com.example.spaghettichat.messagemanager;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.spaghettichat.R;
+
+public class MessageManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_message_manager_view);
+
+    }
+}
