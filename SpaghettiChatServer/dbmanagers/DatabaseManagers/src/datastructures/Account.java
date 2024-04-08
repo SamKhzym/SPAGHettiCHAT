0e2b4869c6491db805e3290aae7ac17cb67f433d
Sam Khzym 👈😎👈 <smryk7@gmail.com>
2024-04-08 07:36:28 -0400

    reworked database definition files slightly to make things a bit nicer to deal with, wrote initial revision of database managers for handling insertions and queries. more work needed.

---
+++ SpaghettiChatServer/dbmanagers/DatabaseManagers/src/datastructures/Account.java
@@ -0,0 +1,29 @@
+package datastructures;
+
+public class Account {
+
+    private String employeeId, firstName, lastName;
+    private boolean isAdmin;
+
+    public Account(String employeeId, String firstName, String lastName, boolean isAdmin) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getEmployeeId() { return employeeId; }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean getIsAdmin() { return isAdmin; }
+
+    public String getFullName() { return firstName + " " + lastName; }
+
+}
